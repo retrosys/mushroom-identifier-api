84c29d482ac88c6d19168a204989fc19f704944c
Cod_ <pierreporetti@gmail.com>
2025-02-16 16:39:33 +0100

    Create cors.go

---
+++ handlers/cors.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+    "net/http"
+)
+
+func EnableCORS(next http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "*")
+        w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+        if r.Method == "OPTIONS" {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
+
+        next(w, r)
+    }
+}
